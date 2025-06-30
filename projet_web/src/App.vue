de89fa229c7a9b538ae9943ba251471b37f84c43
ninjageek78 <seixas.vicente78@gmail.com>
2025-06-30 20:00:49 +0200

    recuperation des mails

--- projet_web/src/App.vue
+++ projet_web/src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import SigninButton from './components/SigninButton.vue'
 import BaseLayout from './components/BaseLayout.vue'
 import HomePage from './pages/HomePage.vue'
 </script>
 
 <template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <SigninButton :user="user" @userChanged="handleUserChanged"/>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
   <base-layout>
     <home-page />
   </base-layout>
