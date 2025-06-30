ae3527f88cbe33dc5eb18daae12746852c73d1a2
Manon <manon.gautier.web@gmail.com>
2025-06-30 20:37:25 +0200

    add basic component from TPs

---
+++ projet_web/src/components/BaseLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="layout">
+    <base-header />
+    <div class="content">
+      <slot />
+    </div>
+    <base-footer />
+  </div>
+</template>
+
+<script setup>
+import BaseHeader from './BaseHeader.vue'
+import BaseFooter from './BaseFooter.vue'
+</script>
+
+<style scoped>
+#layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.content {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+}
+</style>
