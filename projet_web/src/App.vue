f6cffed44446ff5bc3dddd3ff2a66a9346758196
ninjageek78 <seixas.vicente78@gmail.com>
2025-06-30 21:24:19 +0200

    Config de base

--- projet_web/src/App.vue
+++ projet_web/src/App.vue
@@ -2,15 +2,20 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import SigninButton from './components/SigninButton.vue'
+import UserProfile from './components/UserProfile.vue'
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+const store = useStore();
+const user = computed(() => store.getters.user);
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-      <SigninButton :user="user" @userChanged="handleUserChanged"/>
+      <SigninButton/>
+      <UserProfile />
     </div>
   </header>
 
--- projet_web/src/components/SigninButton.vue
+++ projet_web/src/components/SigninButton.vue
@@ -1,12 +1,8 @@
 <script setup>
-import { defineProps, defineEmits } from 'vue';
+import { useStore } from 'vuex';
 import * as msal from '@azure/msal-browser';
 
-const props = defineProps({
-  user: Object
-});
-
-const emit = defineEmits(['userChanged']);
+const store = useStore();
 
 const msalInstance = new msal.PublicClientApplication({
   auth: {
@@ -19,33 +15,12 @@ const msalInstance = new msal.PublicClientApplication({
 
 const signIn = async () => {
   await msalInstance.initialize();
-  const requestedScopes = {
-    scopes: ["User.Read"]
-  };
-  const authResult = await msalInstance.loginPopup(requestedScopes);
+  const authResult = await msalInstance.loginPopup({ scopes: ["User.Read"] });
   msalInstance.setActiveAccount(authResult.account);
-  emit('userChanged', authResult.account);
+  store.commit('setUser', authResult.account); // 👈 this sets user in Vuex
 };
 </script>
 
 <template>
   <button @click="signIn">Sign In with Microsoft</button>
-  <div v-if="user">
-    <p>Welcome, {{ user.name }}</p>
-  </div>
 </template>
-
-<style scoped>
-button {
-  padding: 10px 20px;
-  background-color: #4CAF50;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-button:hover {
-  background-color: #45a049;
-}
-</style>
